ffc67bd8b745b148ba3f1500c1cb11a42706b56d
amina.kausar <amina.kausar@esf1-a1-0059.students.ethacademy.it>
2025-03-13 16:34:16 +0100

    added directory pages/templates/pages

--- blueprints/public.py
+++ blueprints/public.py
@@ -1,3 +1,15 @@
 from flask import Blueprint
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
 
-public_bp= Blueprint('public', __name__)
+public_bp = Blueprint('pages', __name__,
+                        template_folder='templates')
+
+@public_bp.route('/pages/', defaults={'page': 'index'})
+@public_bp.route('/<page>')
+def show(page):
+    try:
+        return render_template(f'pages/{page}.html')
+    except TemplateNotFound:
+        abort(404)
+
